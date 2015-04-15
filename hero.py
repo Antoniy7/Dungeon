fd45ceb60161a2627068c558f76f0c75188dfe47
Antoniy7 <antoniy_pavlov@abv.bg>
2015-04-15 22:04:43 +0300

    add

--- hero.py
+++ hero.py
@@ -5,10 +5,14 @@ class Hero:
 		self.health=health
 		self.mana=mana
 		self.mana_regeneration_rate=mana_regeneration_rate
+		self.weapon=weapon
+		self.spell=spell
 
 	def known_as(self):
 		 message="{hero_name} {hero_title}"
         return message.format(self.name, self.title)
+    
+
     def is_alive(self):
     	if self.health<=0 :
     		return False
@@ -22,9 +26,43 @@ class Hero:
     	return self.mana
 
     def can_cast():
-    	if mana>0:
+    	if self.mana>0:
     		return True
     	else :
     		return False
 
-    
+    def take_damage(self,damage_points):
+    	self.health-=damage_points
+    	if self.health<0:
+    		self.health=0
+
+    def take_healing(self,healing_points):
+    	if self.health<0:
+    		return False
+    	if self.health is 100:
+    		return 
+    	self.health+=healing_points
+    	return True
+
+    def take_mana(self,mana_points):
+    	if self.mana is 100:
+    		return False
+    	if hero_move() is True:
+    		self.mana += self.mana_regeneration_rate
+
+    	self.mana+=mana_points
+
+    def equip(self,weapon):
+    	self.weapon=weapon
+
+    def learn(self,spell):
+    	self.spell=spell
+
+    def attack(self):
+    	
+
+
+
+
+
+
