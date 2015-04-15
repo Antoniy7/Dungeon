a9885f811e57d3b9921bcd329c19f40a52aff371
Antoniy7 <antoniy_pavlov@abv.bg>
2015-04-15 21:44:37 +0300

    add

--- hero.py
+++ hero.py
@@ -1,1 +1,30 @@
-print("dsa")
+class Hero:
+	def __init__ (self,name="Baron",title="Dragonslayer",health=100,mana=100,mana_regeneration_rate=2):
+		self.name=name
+		self.title=title
+		self.health=health
+		self.mana=mana
+		self.mana_regeneration_rate=mana_regeneration_rate
+
+	def known_as(self):
+		 message="{hero_name} {hero_title}"
+        return message.format(self.name, self.title)
+    def is_alive(self):
+    	if self.health<=0 :
+    		return False
+    	else :
+    		return True
+
+    def get_health(self):
+    	return self.health
+    
+    def get_mana(self):
+    	return self.mana
+
+    def can_cast():
+    	if mana>0:
+    		return True
+    	else :
+    		return False
+
+    
