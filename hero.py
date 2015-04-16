a438942a077b27324c27a3548f33e025dd1d1737
Antoniy7 <antoniy_pavlov@abv.bg>
2015-04-16 19:57:28 +0300

    NEWEST

--- hero.py
+++ hero.py
@@ -1,14 +1,35 @@
-
+import Weapon from weapon
 import Man from man
 
-class Hero(Man):
+class Hero(Man,Weapon):
      def __init__ (self,name="Baron",title="Dragonslayer",mana_regeneration_rate=2):
         super().__init__(None,health,mana)
         self.mana_regeneration_rate=mana_regeneration_rate
         self.name=name
         self.title=title
+        self.weapon=None
+        self.spell=None
 
-    
+    def has_weapon(self):
+        return self.equipped_weapon
+
+    def equip(self,weapon):
+        self.weapon=weapon
+
+    def learn(self,spell):
+        self.spell=spell
+
+
+    def attack(self,attacking_object,weapon):
+        if weapon.name is "weapon":
+            return weapon.damage
+
+        if weapon.name is "magic":
+            return weapon.damage_points
+        
+
+
+
     def take_mana(self,mana_points):
         super().take_mana(None,health,mana)
         if hero_move() is True:
