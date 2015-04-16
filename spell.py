577e87939b497332a9fbdf63b72edf9b3fa5bee1
Presian Danailov <presian@gbg.bg>
2015-04-16 20:14:32 +0300

    Fix somme classes

--- spell.py
+++ spell.py
@@ -1,16 +1,16 @@
-import Weapon from weapon
-import Man from man
-class Spell(Weapon,Man):
-    def __init__(self,name,damage,mana_cost,cast_range):
-        super().__init__(None,name,damage)
-        self.__mana_cost=mana_cost
-        self.__cast_range=cast_range
+from weapon import Weapon
+from man import Man
 
-    def mana_cost(self):
-        if self.mana > mana_cost:
-            return False
 
-        else self.mana-=mana_cost
+class Spell(Weapon, Man):
 
-    def cast_range(self):
-        
+    def __init__(self, name, damage, mana_cost, cast_range):
+        Weapon.__init__(self, name, damage)
+        self.__mana_cost = mana_cost
+        self.__cast_range = cast_range
+
+    def get_name(self):
+        return self.__mana
+
+    def get_cast_range(self):
+        return self.__cast_range
--- weapon.py
+++ weapon.py
@@ -1,15 +1,11 @@
-#import Man from man
-#import Hero from hero
+class Weapon:
 
-class Weapon():
-	def __init__(self,name,damage):
-		#super().__init__(None,name,title,health,mana,mana_regeneration_rate)
-		#self.weapon_damage=weapon_damage
-		#self.weapon_name=weapon_name
-		self.__name=name
-		self.__damage=damage
+    def __init__(self, name, damage):
+        self.__name = name
+        self.__damage = damage
 
-	def 
+    def get_name(self):
+        return self.__name
 
-
-
+    def get_damage(self):
+        return self.__damage
