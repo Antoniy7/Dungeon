04d6d5842754010b338e744b411500894e70bd34
Antoniy7 <antoniy_pavlov@abv.bg>
2015-04-20 19:34:45 +0300

    add

--- dungeon.py
+++ dungeon.py
@@ -2,15 +2,19 @@ import Hero from hero
 
 class Dungeon:
 
-    def __init__(self, filename):
+    def __init__(self, hero):
         self.list=[]
+        self.hero=hero
         self.current_x=None
         self.current_y=None
+
+     def map_reading(self, path):
         with open(filename, "r") as in_file:
             lines = in_file.read().splitlines()
-            self.dungeon_map = []
             for line in lines:
-                self.dungeon_map.append(list(line))
+                self.list.append(line)
+    return self.list
+              
 
     def print_map(self):
         for line in self.list:
@@ -28,7 +32,29 @@ class Dungeon:
                     self.current_y = j
  
     return True
+ 
+    def move_hero(self, direction):
+        if direction == "up":
+            if self.current_x == 0:
+                return False
+        new_x = self.current_x - 1
+        new_y = self.current_y
 
-        
+        if direction == "down":
+            if self.current_x == len(self.list):
+                return False
 
+        new_x = self.current_x + 1
+        new_y = self.current_y
 
+        if direction == "left":
+            if self.current_y == 0:
+                return False
+        new_y = self.current_y - 1
+        new_x = self.current_x
+
+        if direction == "right":
+            if self.current_y == len(self.list[0]):
+                return False
+        new_x = self.current_x
+        new_y = self.current_y + 1
