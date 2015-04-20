674b1dfaee51bfa2cf6002252aece1d7e2bc5175
Presian Danailov <presian@gbg.bg>
2015-04-20 20:44:23 +0300

    Add some constants

--- man.py
+++ man.py
@@ -1,15 +1,16 @@
 class Man:
 
+    HEALTH_MIN = 0
+    HEALTH_MAX = 100
+    MANA_MIN = 0
+    MANA_MAX = 100
+
     def __init__(self, health=100, mana=100):
         self.__health = health
         self.__mana = mana
-        self.HEALTH_MIN = 0
-        self.HEALTH_MAX = 100
-        self.MANA_MIN = 0
-        self.MANA_MAX = 100
 
     def is_alive(self):
-        return self.__health > self.HEALTH_MIN
+        return self.__health > Man.HEALTH_MIN
 
     def get_health(self):
         return self.__health
@@ -18,16 +19,16 @@ class Man:
         return self.__mana
 
     def can_cast(self):
-        return self.mana > self.MANA_MIN
+        return self.mana > Man.MANA_MIN
 
     def take_mana(self, mana_points):
-        if self.__mana + mana_points >= self.MANA_MAX:
-            self.__mana = self.MANA_MAX
+        if self.__mana + mana_points >= Man.MANA_MAX:
+            self.__mana = Man.MANA_MAX
         else:
             self.__mana += mana_points
 
     def take_healing(self, healing_points):
-        if self.__health + healing_points >= self.HEALTH_MAX:
-            self.__health = self.HEALTH_MAX
+        if self.__health + healing_points >= Man.HEALTH_MAX:
+            self.__health = Man.HEALTH_MAX
         else:
             self.__health += healing_points
